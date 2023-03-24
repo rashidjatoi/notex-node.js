973d2ffc98946eca500882ca4455f423fac6532a
Abdullah-Al-Jubayer <jabdullahal390@gmail.com>
2023-03-24 15:44:56 +0500

    new changes

--- index.js
+++ index.js
@@ -9,22 +9,18 @@ const app = express();
 // port
 const PORT = process.env.PORT || 5000;
 
-
 // middleware
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-
 // home route
 app.get("/", (req, res) => {
-  res.send("This is the Home Page");
+  const response = { statuscode: res.statusCode, message: "Api works! " };
+  res.json(response);
 });
-
 
 // router
 app.use("/notes", notesRouter);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT:  ${PORT}`);
